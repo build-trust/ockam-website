afd3fa6bf2d096fec06ae67f273ad42b6669035c
Piotr Sobieszczański <piotr.s@masterborn.com>
2020-01-27 13:47:00 +0100

    chore: correct creation of URL_PATH

--- scripts/get-depended-repos.sh
+++ scripts/get-depended-repos.sh
@@ -25,7 +25,7 @@ do
   REPO_PATH=src/content/$URL_PATH
   _message "Cloning $ORGANIZATION/$REPO_NAME.."
   git clone -n https://$TOKEN@github.com/$ORGANIZATION/$REPO_NAME /tmp/$REPO_NAME
-  mkdir -p src/content/ # make sure subfolder exists
+  mkdir -p $REPO_PATH # make sure subfolder(s) exists
   mv /tmp/$REPO_NAME$SRC_DIR $REPO_PATH
   rm -rf /tmp/$REPO_NAME # clean it
   echo
