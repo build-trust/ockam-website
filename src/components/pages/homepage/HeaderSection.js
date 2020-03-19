d0cc7a13796c0b9a049b5f076b56fd320ef65808
miedziak <miedziak@gmail.com>
2020-03-19 16:45:21 +0100

    feat: Replace icon on homepage hero section

--- src/components/pages/homepage/HeaderSection.js
+++ src/components/pages/homepage/HeaderSection.js
@@ -1,19 +1,30 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-import Button from '../../Button';
-import Link from '../../Link';
 import Text from '../../Text';
 import Heading from '../../Heading';
 import homepageHeaderImage from '../../../assets/homepage/header-section-image.svg';
 import HeaderGridSection from '../HeaderGridSection';
-import CheckedListItem from "../../CheckedListItem";
-import useModal from "../../../hooks/useModal";
-import ContactModal from "../../../modals/ContactModal";
 import Subheading from "../Subheading";
+import IconListItem from "../../IconLisItem";
+import { ReactComponent as MessageIcon } from '../../../assets/message-icon.svg';
+import  { ReactComponent as HandsIcon } from '../../../assets/hands-icon.svg';
+import  { ReactComponent as FingerPrint } from '../../../assets/fingerprint-icon.svg';
 
+
+
+const ListItem = ({ children, icon, ...rest }) => <IconListItem icon={icon} iconStyle={{ marginTop: 1}} {...rest}>{ children }</IconListItem>;
+
+ListItem.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.arrayOf(PropTypes.node),
+  ]).isRequired,
+  icon: PropTypes.string.isRequired,
+};
 
 const HeaderSection = () => {
-  const [, showContactModal] = useModal(ContactModal);
+
   return (
     <HeaderGridSection image={homepageHeaderImage}>
       <Subheading mb={3}>Ockam makes it as simple as it should be.</Subheading>
@@ -23,21 +34,21 @@ const HeaderSection = () => {
         you can trust.
       </Heading>
       <Text mt={4}>
-      Your customers expect security and data privacy from your products.
-      Ockam’s developer tools enable you to:
+        Your customers expect security and data privacy from your products.
+        Ockam’s developer tools enable you to:
       </Text>
       <div>
-        <CheckedListItem>
+        <ListItem icon={HandsIcon}>
           Send end-to-end encrypted messages through your connected system,
-        </CheckedListItem>
-        <CheckedListItem>
+        </ListItem>
+        <ListItem icon={MessageIcon}>
           Establish authenticated channels between endpoints regardless of the
           transports or route,
-        </CheckedListItem>
-        <CheckedListItem>
+        </ListItem>
+        <ListItem icon={FingerPrint}>
           Create and manage cryptographic keys, unique device identifiers, and
           verifiable credentials in your connected environments.
-        </CheckedListItem>
+        </ListItem>
       </div>
     </HeaderGridSection>
 )};
