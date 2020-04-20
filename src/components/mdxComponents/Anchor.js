0407297c9bc960c88315dff148be5b388e02fabd
miedziak <miedziak@gmail.com>
2020-04-20 13:02:12 +0200

    feat: relative links in md files
    Making all relative links ( `./*` ) generated from md files to be relative to current folder,
    not base url of the website.

--- src/components/mdxComponents/Anchor.js
+++ src/components/mdxComponents/Anchor.js
@@ -1,19 +1,42 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
+import dropRight from 'lodash/dropRight';
+import isString from 'lodash/isString';
 
 import Link from '../Link';
+import useLocation from "../../hooks/useLocation";
+
 
 const StyledLink = styled(Link)`
   text-decoration: underline;
   color: ${props => props.theme.colors.primary};
 `;
 
+const isRelativePath = path => {
+  return path.match(/^\.\/+/);
+}
+
+const getBasePathToFolder = path => {
+  let basePath = path;
+  if(basePath.charAt(basePath.length) === '/') basePath = basePath.substring(0, basePath.length);
+  return dropRight(basePath.split('/'), 2).join('/');
+}
+
 const AnchorTag = ({ children: link, href }) => {
-  if (link) {
-    return <StyledLink to={href}>{link}</StyledLink>;
+  const isAnchor = isString(href) && href.charAt(0) === '#'
+  const location = useLocation();
+
+  if (!link) return null;
+
+  let to = href;
+  if(isRelativePath(to) && !isAnchor) {
+    const toPath = to.substr(2);
+    const basePath = getBasePathToFolder(location.pathname);
+    to = `${basePath}/${toPath}`
   }
-  return null;
+  return <StyledLink to={to}>{link}</StyledLink>;
+
 };
 
 AnchorTag.propTypes = {
