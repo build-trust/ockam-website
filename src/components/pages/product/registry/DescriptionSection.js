f3ab212fd8a760127b31fa3b022a9c5bf2343bae
Bartek <bartek.codes@gmail.com>
2020-09-24 11:29:22 +0200

    content: router and registry design revision

--- src/components/pages/product/registry/DescriptionSection.js
+++ src/components/pages/product/registry/DescriptionSection.js
@@ -1,13 +1,39 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
+import { media } from '../../../../utils/emotion';
 import DefaultGridSection from '../../DefaultGridSection';
-import registryGraphics from '../../../../assets/product/subpage-registry-diagram.svg';
+import registryGraphics from '../../../../assets/product/registry-hero.svg';
+import registryBox from '../../../../assets/product/registry-box.svg';
+import registryBoxMobile from '../../../../assets/product/registry-box-mobile.svg';
 import Text from '../../../Text';
 import CheckedListItem from '../../../CheckedListItem';
 
 const SpacingContainer = styled('div')`
   margin-top: 9rem;
+`;
+
+const ImageBoxContainer = styled('div')`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  max-width: 100%;
+`;
+
+const ImageBox = styled('div')`
+  display: flex;
+  margin-bottom: 5rem;
+  background: transparent url(${registryBoxMobile}) no-repeat center center;
+  background-size: contain;
+  min-height: 48rem;
+  ${media.tablet`
+    margin-bottom: 9.6rem;
+    background: transparent url(${registryBox}) no-repeat center center;
+    background-size: contain;
+    min-height: 27rem;
+  `}
+  width: 100%;
+  max-width: 110rem;
 `;
 
 const DescriptionSection = () => {
@@ -40,6 +66,9 @@ const DescriptionSection = () => {
           A cloud based HSM (Hardware Security Module)
         </CheckedListItem>
       </DefaultGridSection>
+      <ImageBoxContainer>
+        <ImageBox />
+      </ImageBoxContainer>
     </SpacingContainer>
   );
 };
--- src/components/pages/product/router/DescriptionSection.js
+++ src/components/pages/product/router/DescriptionSection.js
@@ -1,126 +1,73 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-import Heading from '../../../Heading';
-import Subheading from '../../Subheading';
 import Text from '../../../Text';
+import DefaultGridSection from '../../DefaultGridSection';
 import CheckedListItem from '../../../CheckedListItem';
-import PageSection from '../../PageSection';
 import { media } from '../../../../utils/emotion';
-import routerDiagram from '../../../../assets/product/subpage-router-diagram.svg';
-import routerDiagramEdged from '../../../../assets/product/subpage-router-diagram-edged.svg';
-import routerDiagramMobile from '../../../../assets/product/subpage-router-diagram-mobile.svg';
-import AnimateOnScroll from '../../../AnimateOnScroll';
-import Image from '../../../Image';
-
-const Container = styled('div')`
-  display: flex;
-  flex-direction: column;
-
-  ${media.desktop`
-    flex-direction: row;
-      align-items: center;
-  `}
-`;
-
-const DescriptionBox = styled('div')`
-  flex: 1;
-  ${media.desktop`
-    flex: 5;
-  `}
-`;
-
-const ImageBox = styled('div')`
-  display: none;
-  position: relative;
-
-  ${media.desktop`
-    display:block;
-    flex: 6;
-    margin-left: 4rem;
-    height: 480px;
-    width: 100%;
-  `}
-  ${media.ultraWide`
-    height: 567px;
-  `};
-`;
-
-const DesktopImage = styled('div')`
-  background-image: url(${routerDiagram}), url(${routerDiagramEdged});
-  background-repeat: no-repeat, no-repeat;
-  background-position: 0 center, 637px center;
-  background-size: contain;
-  position: absolute;
-  height: 100%;
-  width: 100vw;
-`;
-
-const ImageBoxMobile = styled(Image)`
-  width: 100%;
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-  max-width: 50rem;
-  ${media.desktop`
-    display:none;
-  `}
-`;
-
-const RouterSection = styled(PageSection)`
-  overflow-x: hidden;
-`;
+import routerGraphics from '../../../../assets/product/router-hero.svg';
+import routerBox from '../../../../assets/product/router-box.svg';
+import routerBoxMobile from '../../../../assets/product/router-box-mobile.svg';
 
 const SpacingContainer = styled('div')`
   margin-top: 9rem;
 `;
 
-const SubheadingWrapper = styled('div')`
-  max-width: 80%;
+const ImageBoxContainer = styled('div')`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  max-width: 100%;
+`;
+
+const ImageBox = styled('div')`
+  display: flex;
+  margin-bottom: 5rem;
+  background: transparent url(${routerBoxMobile}) no-repeat center center;
+  background-size: contain;
+  min-height: 48rem;
+  ${media.tablet`
+    margin-bottom: 9.6rem;
+    background: transparent url(${routerBox}) no-repeat center center;
+    background-size: contain;
+    min-height: 27rem;
+  `}
+  width: 100%;
+  max-width: 110rem;
 `;
 
 const DescriptionSection = () => {
   return (
     <SpacingContainer>
-      <RouterSection>
-        <AnimateOnScroll transformY animateOnce>
-          <Container>
-            <DescriptionBox>
-              <SubheadingWrapper>
-                <Subheading>
-                  Connect your distributed system with ease.
-                </Subheading>
-              </SubheadingWrapper>
-              <Heading linked as="h2">
-                Ockam Router
-              </Heading>
-              <Text>
-                Ockam Router is a service hosted in a cloud environment that
-                makes it easy to route messages between an unlimited number of
-                Ockam authenticated connections in your business.
-              </Text>
-              <CheckedListItem>
-                Ockam Router Service
-                <Text color="caption">a cloud based routing service</Text>
-              </CheckedListItem>
-              <CheckedListItem>
-                Ockam Cloud SDK
-                <Text color="caption">
-                  allows other services and environments to securely connect
-                  with an Ockam Router.
-                </Text>
-              </CheckedListItem>
-              <CheckedListItem>
-                A cloud based HSM (Hardware Security Module)
-              </CheckedListItem>
-            </DescriptionBox>
-            <ImageBox>
-              <DesktopImage />
-            </ImageBox>
-            <ImageBoxMobile src={routerDiagramMobile} />
-          </Container>
-        </AnimateOnScroll>
-      </RouterSection>
+      <DefaultGridSection
+        image={routerGraphics}
+        direction="imageOnRight"
+        title="Ockam Router"
+        subtitle="Connect your distributed system with ease."
+      >
+        <Text>
+          Ockam Router is a service hosted in a cloud environment that makes it
+          easy to route messages between an unlimited number of Ockam
+          authenticated connections in your business.
+        </Text>
+        <CheckedListItem>
+          Ockam Router Service
+          <Text color="caption">a cloud based routing service</Text>
+        </CheckedListItem>
+        <CheckedListItem>
+          Ockam Cloud SDK
+          <Text color="caption">
+            allows other services and environments to authenticate and message
+            with an Ockam Router.
+          </Text>
+        </CheckedListItem>
+        <CheckedListItem>
+          A cloud based HSM (Hardware Security Module)
+        </CheckedListItem>
+      </DefaultGridSection>
+      <ImageBoxContainer>
+        <ImageBox />
+      </ImageBoxContainer>
     </SpacingContainer>
   );
 };
