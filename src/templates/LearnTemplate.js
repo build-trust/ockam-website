87b16be8cb826c4fa69a597ffef82db70e578e25
Piotr Sobieszczański <piotr.sobieszczanski@gmail.com>
2021-02-25 15:03:31 +0100

    fix: add elixir language highlighting support

--- src/templates/LearnTemplate.js
+++ src/templates/LearnTemplate.js
@@ -21,6 +21,7 @@ import Prism from 'prism-react-renderer/prism';
 (typeof global !== 'undefined' ? global : window).Prism = Prism;
 
 require('prismjs/components/prism-rust');
+require('prismjs/components/prism-elixir');
 
 const Content = styled.div`
   display: flex;
