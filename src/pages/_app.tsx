eeae0190a57af699c7318e30ba6c3ddac0fc23e8
Łukasz Tucholski <luk.tucholski@gmail.com>
2022-07-29 10:44:32 +0200

    fix: stylesheet import in custom _document file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, ReactElement, ReactNode, useMemo } from 'react';
+import { FunctionComponent, ReactElement, ReactNode, useMemo } from 'react';
 import Head from 'next/head';
 import { AppProps } from 'next/app';
 import NextNprogress from 'nextjs-progressbar';
@@ -51,16 +51,6 @@ const App: FunctionComponent<AppPropsWithLayout> = (props) => {
         <link rel="canonical" href={canonicalUrl} key={KEYS.canonicalUrl} />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro:wght@400;500;600;700;800&display=swap"
-          rel="stylesheet"
-        />
       </Head>
       <StylesProvider>
         <MobileNavbarProvider>
---
+++ src/pages/_document.tsx
@@ -0,0 +1,24 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+const CustomDocument = (): JSX.Element => (
+  <Html lang="en">
+    <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro:wght@400;500;600;700;800&display=swap"
+        rel="stylesheet"
+      />
+    </Head>
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default CustomDocument;
