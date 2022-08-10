5b9e3c751caa968cdb480716db52b0361ab54531
Łukasz Tucholski <luk.tucholski@gmail.com>
2022-08-10 11:05:22 +0200

    build: upgrade next.js; optimize bundle

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-const withPlugins = require('next-compose-plugins');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
@@ -8,4 +7,7 @@ const nextConfig = {
   images: { formats: ['image/webp'] },
 };
 
-module.exports = withPlugins([withSvgr, withBundleAnalyzer], nextConfig);
+module.exports = (_phase, { defaultPlugin }) => {
+  const plugins = [withSvgr, withBundleAnalyzer];
+  return plugins.reduce((acc, plugin) => plugin(acc), { ...nextConfig });
+};
