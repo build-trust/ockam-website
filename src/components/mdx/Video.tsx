bfb49c0be8643274f7cdb04bd6b84624f7648d08
Glenn Gillen <github@gln.io>
2023-03-03 10:11:37 +1100

    feat: adding video to confluent announcement

--- src/components/mdx/Video.tsx
+++ src/components/mdx/Video.tsx
@@ -8,7 +8,7 @@ const Video: FC<Props> = ({ src, track }) => (
   <video controls>
     <source src={src} type="video/mp4" />
     Your browser does not support the video tag.
-    <track label="English" kind="captions" src={track} default />
+    <track label="English" kind="captions" src={track} />
   </video>
 );
 export default Video;
