b5bb4031809730415ce56d59beea12f7a596d28c
Glenn Gillen <github@gln.io>
2023-03-09 15:23:23 +1100

    fix: remove linting rule to allow build to continue

--- src/components/mdx/CodeBlock.tsx
+++ src/components/mdx/CodeBlock.tsx
@@ -3,7 +3,6 @@ import { HTMLAttributes } from 'react';
 import CopyToClipboard from '@components/CopyToClipboard';
 
 const CodeBlock = ({ children, ...props }: HTMLAttributes<HTMLElement>): JSX.Element => {
-  // eslint-disable-next-line react/jsx-no-useless-fragment
   const button = props.className ? (
     <CopyToClipboard position="absolute" top={5} right={5} />
   ) : (
