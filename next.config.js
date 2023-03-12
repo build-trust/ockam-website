01767047c6f8bbc9750fcb274ffde8c1812c90a7
Glenn Gillen <github@gln.io>
2023-03-12 15:26:40 +1100

    fix: redirects to keep search console happy

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-const {redirectJobs, redirectDocs} = require('./lib/redirects')
+const { redirectJobs, redirectDocs } = require('./lib/redirects');
 const withSvgr = require('next-svgr');
 
 const nextConfig = {
@@ -23,11 +23,38 @@ const nextConfig = {
       destination: '/open-source',
       permanent: true,
     },
+    {
+      source: '/learn/blog/trust_influxdb/',
+      destination: '/blog/connect_private_influxdb',
+      permanent: true,
+    },
+    {
+      source: '/learn/how-to-guides/high-performance-team/values_and_virtues_on_the_Ockam_Team/',
+      destination: '/blog/high_performance_team',
+      permanent: true,
+    },
+    {
+      source: '/learn/guides/contributions/CONTRIBUTING',
+      destination:
+        'https://github.com/build-trust/.github/blob/main/CONTRIBUTING.md#contributing-to-ockam-on-github',
+      permanent: true,
+    },
+    {
+      source: '/learn/how-to-guides/contributing/CONTRIBUTING',
+      destination:
+        'https://github.com/build-trust/.github/blob/main/CONTRIBUTING.md#contributing-to-ockam-on-github',
+      permanent: true,
+    },
+    {
+      source: '/learn/how-to-guides/contributing/CONTRIBUTING/',
+      destination:
+        'https://github.com/build-trust/.github/blob/main/CONTRIBUTING.md#contributing-to-ockam-on-github',
+      permanent: true,
+    },
     ...redirectDocs,
-    ...redirectJobs
-    
+    ...redirectJobs,
   ],
-}
+};
 
 module.exports = (_phase, { defaultPlugin }) => {
   const plugins = [withSvgr, withBundleAnalyzer];
