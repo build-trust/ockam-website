00442fc573e301c4a9f8f9111901d3e3fdd9fbf1
Matthew Gregory <matt@ockam.io>
2023-04-03 01:18:08 +0000

    fix: deleted missed references

--- src/views/homepage/index.ts
+++ src/views/homepage/index.ts
@@ -1,7 +1,5 @@
 export { default as Hero } from './Hero';
 export { default as GetStarted } from './GetStarted';
-export { default as Tools } from './Tools';
-export { default as Infrastructure } from './Infrastructure';
 export { default as Features } from './Features';
 export { default as Cases } from './Cases';
 export { default as Packages } from './Packages';
