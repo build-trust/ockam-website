7e655f7f2fdd3ccc8f5034e581d140e75c0c60fd
Glenn Gillen <github@gln.io>
2023-04-05 15:49:49 +1000

    fix: forgot to add a file

---
+++ src/views/homepage/RotatingHeading.tsx
@@ -0,0 +1,54 @@
+import { FC } from 'react';
+import { Box, Heading, useTheme } from '@chakra-ui/react';
+
+import RotatingText from '@root/components/RotatingText';
+
+const RotatingHeading: FC = () => {
+  const { gradients } = useTheme();
+  return (
+    <Box my={30}>
+      <Heading
+        as="h1"
+        size={{ base: 'md', lg: 'h2' }}
+        fontWeight="extrabold"
+        textAlign="center"
+        color="white"
+      >
+        What if connecting{' '}
+        <RotatingText
+          interval={3000}
+          delay={4000}
+          words={[
+            'Kafka',
+            'InfluxDB',
+            'services',
+            'devices',
+            'applications',
+            'anything',
+            'everything',
+          ]}
+          styles={{
+            backgroundImage: gradients.primary,
+            WebkitBackgroundClip: 'text',
+            backgroundClip: 'text',
+            color: 'transparent',
+          }}
+        />{' '}
+        was as{' '}
+        <RotatingText
+          interval={4000}
+          delay={8000}
+          words={['secure', 'simple', 'trusted', 'private']}
+          styles={{
+            backgroundImage: gradients.primary,
+            WebkitBackgroundClip: 'text',
+            backgroundClip: 'text',
+            color: 'transparent',
+          }}
+        />{' '}
+        as a conversation&nbsp;with your&nbsp;friends?
+      </Heading>
+    </Box>
+  );
+};
+export default RotatingHeading;
