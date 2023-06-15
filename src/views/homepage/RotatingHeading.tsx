227cd1143c09e6e10b95c843659575659524c241
Glenn Gillen <glenn@ockam.io>
2023-06-15 20:49:08 +1000

    content: removing old conversation metaphor

--- src/views/homepage/RotatingHeading.tsx
+++ src/views/homepage/RotatingHeading.tsx
@@ -1,58 +1,20 @@
 import { FC } from 'react';
-import { Box, Heading, useTheme } from '@chakra-ui/react';
+import { Box, Heading } from '@chakra-ui/react';
 
-import RotatingText from '@root/components/RotatingText';
-
-const RotatingHeading: FC = () => {
-  const { gradients } = useTheme();
-  return (
-    <Box my={30}>
-      <Heading
-        as="h1"
-        size={{ base: 'md', lg: 'h2' }}
-        fontWeight="extrabold"
-        textAlign="center"
-        color="white"
-        lineHeight={{ base: 1, lg: 1.2 }}
-      >
-        What if connecting{' '}
-        <RotatingText
-          interval={3000}
-          delay={4000}
-          words={[
-            'Kafka',
-            'InfluxDB',
-            'services',
-            'devices',
-            'applications',
-            'anything',
-            'everything',
-          ]}
-          styles={{
-            backgroundImage: gradients.primary,
-            WebkitBackgroundClip: 'text',
-            backgroundClip: 'text',
-            color: 'transparent',
-          }}
-        />{' '}
-        <br />
-        was as{' '}
-        <RotatingText
-          interval={4000}
-          delay={8000}
-          words={['secure', 'simple', 'trusted', 'private']}
-          styles={{
-            backgroundImage: gradients.primary,
-            WebkitBackgroundClip: 'text',
-            backgroundClip: 'text',
-            color: 'transparent',
-          }}
-        />{' '}
-        as a conversation
-        <br />
-        with your&nbsp;friends?
-      </Heading>
-    </Box>
-  );
-};
+const RotatingHeading: FC = () => (
+  <Box my={30}>
+    <Heading
+      as="h1"
+      size={{ base: 'md', lg: 'h2' }}
+      fontWeight="extrabold"
+      textAlign="center"
+      color="white"
+      lineHeight={{ base: 1, lg: 1.2 }}
+    >
+      Ockam enables engineers to build apps
+      <br />
+      that can Trust data-in-motion
+    </Heading>
+  </Box>
+);
 export default RotatingHeading;
