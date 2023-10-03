e2f1ac5f8b94f8f43d1d2fda2357ee8b0ff1d7a6
Glenn Gillen <glenn@ockam.io>
2023-10-03 21:51:41 +1100

    fix: switch to legacy image post upgrade

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -1,5 +1,5 @@
 import { ImageResponse } from '@vercel/og';
-import Image from 'next/image';
+import Image from 'next/legacy/image';
 // eslint-disable-next-line @next/next/no-server-import-in-page
 import { NextRequest } from 'next/server';
 
