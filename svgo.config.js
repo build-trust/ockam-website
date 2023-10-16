5889ff546dae80214f8ee16911e861e05cfb92ce
Glenn Gillen <glenn@ockam.io>
2023-10-16 14:28:03 +1100

    fix: update svg deps for cve issue

---
+++ svgo.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  multipass: true, // boolean. false by default
+  plugins: [
+    // {
+    //     name: 'preset-default',
+    //     params: {
+    //         overrides: {
+    //             removeViewBox: false,
+    //             convertShapeToPath: false,
+    //             convertPathData: false,
+    //             convertEllipseToCircle: false,
+    //             cleanupNumericValues: false
+    //         },
+    //     },
+    // },
+  ],
+};
