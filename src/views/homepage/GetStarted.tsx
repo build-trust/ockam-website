54c60e1d152851b3fde97b4d11dc1e2118a863eb
Glenn Gillen <glenn@ockam.io>
2024-01-10 15:38:58 +1100

    feat: update getting started section

--- src/views/homepage/GetStarted.tsx
+++ src/views/homepage/GetStarted.tsx
@@ -1,12 +1,12 @@
 import { FunctionComponent, SVGAttributes } from 'react';
-import { Box, Flex, Container, Text, SimpleGrid, Icon, Heading } from '@chakra-ui/react';
+import { Box, Flex, Container, Text, SimpleGrid, Icon } from '@chakra-ui/react';
 
 import BuildingIcon from '@assets/icons/building.svg';
 import CloudIcon from '@assets/icons/cloud.svg';
 import GitHubIcon from '@assets/icons/github.svg';
 import GreenIconWrapper from '@components/GreenIconWrapper';
 import Card from '@components/Card';
-import { BUILD_DEMO, AWS_MARKETPLACE, DISCUSSION, DISCORD } from '@consts/externalResources';
+import { DISCORD, GITHUB_REPO_OCKAM } from '@consts/externalResources';
 import GAEvents from '@utils/GAEvents';
 import CTALink from '@components/CTALink';
 import Transition from '@root/components/Transition/Transition';
@@ -17,12 +17,6 @@ const GET_STARTED_CARDS = [
     title: 'Community',
     text: 'Join our Open Source community',
     link: [
-      {
-        text: 'GitHub Discussions',
-        href: DISCUSSION.href,
-        isExternal: true,
-        onClick: GAEvents.outboundGithubLink,
-      },
       {
         text: 'Discord',
         href: DISCORD.href,
@@ -37,7 +31,7 @@ const GET_STARTED_CARDS = [
     text: 'Let’s build an application in a step-by-step guide',
     link: {
       text: 'Show me the code',
-      href: BUILD_DEMO.href,
+      href: GITHUB_REPO_OCKAM.href,
       isExternal: true,
       onClick: GAEvents.outboundStartBuildingLink,
     },
@@ -47,10 +41,9 @@ const GET_STARTED_CARDS = [
     title: 'Companies',
     text: 'Purchase Ockam for your company with your AWS account',
     link: {
-      text: 'AWS Marketplace',
+      text: 'See plans & pricing',
       isExternal: true,
-      href: AWS_MARKETPLACE.href,
-      onClick: GAEvents.outboundOrchestratorLink,
+      href: '/pricing',
     },
   },
 ];
@@ -99,27 +92,6 @@ const GetStartedCard: FunctionComponent<GetStartedCardProps> = ({ icon, title, t
 const GetStarted: FunctionComponent = () => (
   <Box bgColor="gray.50" pt={16} pb={{ base: 16, lg: 24 }} zIndex={2}>
     <Container variant="section">
-      <Heading
-        as="h3"
-        size="h3"
-        mb={8}
-        lineHeight={1.3}
-        maxW={{ lg: '31rem' }}
-        alignSelf="center"
-        letterSpacing="-2px"
-      >
-        Trust for Data-in-Motion
-      </Heading>
-      <Text maxW="2xl" fontSize="lg" textAlign="center" mb={6}>
-        Modern applications are distributed and have an unwieldy number of interconnections that
-        must trustfully exchange data. To trust data-in-motion, applications need end-to-end
-        guarantees of data integrity, authenticity, and&nbsp;privacy.
-      </Text>
-      <Text maxW="2xl" fontSize="lg" textAlign="center" mb={18}>
-        Ockam empowers you with simple developer tools to add data guarantees to
-        any&nbsp;application.
-      </Text>
-
       <SimpleGrid
         bgColor="gray.50"
         columns={{ base: 1, lg: 3 }}
