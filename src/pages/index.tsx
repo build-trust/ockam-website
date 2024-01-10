d3dd17a716cc7e2c478cc01874f7d0cb11890be6
Glenn Gillen <glenn@ockam.io>
2024-01-10 15:24:56 +1100

    feat: remove external stats

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,60 +1,16 @@
 import { ReactElement, ReactNode } from 'react';
 import { Box } from '@chakra-ui/react';
 
-import githubApi from '@api/githubApi';
-import cratesApi from '@api/cratesApi';
 import { NextPageWithLayout } from '@typings/NextPageWithLayout';
 import MainLayout from '@layouts/MainLayout';
-import { Hero, GetStarted, Features, Cases, Stats } from '@views/homepage';
-import CONFIG from '@config';
+import { Hero, GetStarted, Features, Cases } from '@views/homepage';
 
-type HomePageProps = {
-  stars?: number;
-  contributors?: number;
-  downloads?: number;
-};
-
-export async function getStaticProps(): Promise<{ props: HomePageProps; revalidate: number }> {
-  try {
-    const { data: repoContentsData } = await githubApi.reposService.getRepoContents(
-      'ockam',
-      '/.github/CONTRIBUTORS.csv'
-    );
-
-    const { data: repoData } = await githubApi.reposService.getRepo('ockam');
-
-    const responses = await cratesApi.cratesService.getProjects(CONFIG.crates.projectsNames);
-    const downloadsSum = responses
-      .map((resp) => resp.data.crate.downloads)
-      .reduce((partialSum, count) => partialSum + count, 0);
-
-    return {
-      props: {
-        stars: repoData?.stargazers_count,
-        contributors:
-          (Buffer.from(repoContentsData?.content, 'base64')
-            .toString()
-            .split(/\r\n|\r|\n/).length || 2) - 2, // minus column names first row and last empty row
-        downloads: downloadsSum,
-      },
-      revalidate: 120,
-    };
-  } catch (e) {
-    // eslint-disable-next-line no-console
-    console.error(e);
-    return { props: {}, revalidate: 120 };
-  }
-}
-
-const HomePage: NextPageWithLayout<HomePageProps> = ({ stars, contributors, downloads }) => (
+const HomePage: NextPageWithLayout = () => (
   <Box pt={{ base: 0 }}>
     <Hero />
     <Features />
     <Cases />
     <GetStarted />
-    {stars && contributors && downloads && (
-      <Stats stars={stars} contributors={contributors} downloads={downloads} />
-    )}
   </Box>
 );
 
--- src/views/homepage/index.ts
+++ src/views/homepage/index.ts
@@ -3,4 +3,3 @@ export { default as GetStarted } from './GetStarted';
 export { default as Features } from './Features';
 export { default as Cases } from './Cases';
 export { default as Packages } from './Packages';
-export { default as Stats } from './Stats';
