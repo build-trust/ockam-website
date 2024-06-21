0cc729bae51b13437d3f75cb2cef492dc9b97b9a
Mateusz Witkowski <mateusz.w@masterborn.com>
2024-06-21 14:18:16 +0200

    feat: add footer links

--- src/layouts/DarkLayout/Footer/index.tsx
+++ src/layouts/DarkLayout/Footer/index.tsx
@@ -1,10 +1,41 @@
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, Heading, Text, VStack, Link } from '@chakra-ui/react';
 import { ReactElement } from 'react';
+import NextLink from 'next/link';
+
+import FOOTER_LINKS from './consts/navLinks';
 
 const Footer = (): ReactElement => (
   <Box as="footer" bg="brand.800" color="white" px={{ base: '0.75rem' }} py={{ base: '2.5rem' }}>
-    <Flex justifyContent={{ base: 'space-between', lg: 'unset' }} maxW="70rem" mx="auto">
-      Content
+    <Flex
+      gap={{ base: '2rem', lg: 'unset' }}
+      justifyContent={{ base: 'normal', lg: 'space-between' }}
+      flexDir={{ base: 'column', lg: 'row' }}
+      maxW="70rem"
+      mx="auto"
+    >
+      {FOOTER_LINKS.map((section) => (
+        <VStack
+          gap={{ base: '1rem', lg: '0.5rem' }}
+          align={{ base: 'flex-start' }}
+          key={section.heading}
+        >
+          <Heading as="h4" color="white" fontSize={{ base: '1rem' }} fontWeight={{ base: 700 }}>
+            {section.heading}
+          </Heading>
+          {section.links.map((link) => (
+            <Link
+              fontSize={{ base: '0.875rem' }}
+              color="white"
+              key={link.name}
+              href={link.href}
+              as={NextLink}
+              fontWeight={{ base: 400 }}
+            >
+              {link.name}
+            </Link>
+          ))}
+        </VStack>
+      ))}
     </Flex>
   </Box>
 );
