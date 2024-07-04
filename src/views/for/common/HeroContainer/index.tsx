c772118ed73cd3335f0b03deda068a5ea2b0457e
Mateusz Witkowski <mateusz.w@masterborn.com>
2024-07-04 13:49:51 +0200

    feat: add hero background

--- src/views/for/common/HeroContainer/index.tsx
+++ src/views/for/common/HeroContainer/index.tsx
@@ -1,12 +1,15 @@
 import { Box, BoxProps, Flex, FlexProps } from '@chakra-ui/react';
 import { ReactElement } from 'react';
 
+import heroBackgroundSrc from './assets/hero-background.svg?url';
+
 interface HeroContainerProps extends BoxProps {}
 export const HeroContainer = ({ children, ...boxProps }: HeroContainerProps): ReactElement => (
   <Box
     py={{ base: '2.5rem', lg: '11.75rem' }}
     px={{ base: '0.75rem' }}
-    background="linear-gradient(180deg, #0D1721 0%, #162535 100%)"
+    backgroundPosition="center bottom"
+    bgImage={`/**/url(${heroBackgroundSrc.src}), linear-gradient(180deg, #0D1721 0%, #162535 100%)`}
     {...boxProps}
   >
     {children}
