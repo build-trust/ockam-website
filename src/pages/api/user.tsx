5c1465960e9221c77031d1c0d0b4f5a0e89d491d
Glenn Gillen <glenn@ockam.io>
2024-07-11 23:12:53 +1000

    feat: return error from api

--- src/pages/api/user.tsx
+++ src/pages/api/user.tsx
@@ -2,16 +2,19 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export default async function handler(request: NextApiRequest, response: NextApiResponse) {
-
-    const res = await fetch(
-        `${process.env.OCKAM_API_BASE_URL}/enrollment?userid=${request.query.userid}`,
-        {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'ockam-api-key': process.env.OCKAM_API_KEY as string,
-            }
-        })
-    const data = await res.json()
-    response.json(data);
+    try {
+        const res = await fetch(
+            `${process.env.OCKAM_API_BASE_URL}/enrollment?userid=${request.query.userid}`,
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'ockam-api-key': process.env.OCKAM_API_KEY as string,
+                }
+            })
+        const data = await res.json()
+        response.json(data);
+    } catch (error) {
+        response.json({ error })
+    }
 }
