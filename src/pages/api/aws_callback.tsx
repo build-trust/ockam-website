5a0695bca926a8a482545214e79e76db818ce8bc
Chris Hildebrand <git@ckhrysze.net>
2024-07-14 20:57:11 -0400

    feat: handle aws marketplace fulfillment as post

--- src/pages/api/aws_callback.tsx
+++ src/pages/api/aws_callback.tsx
@@ -3,17 +3,25 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export default async function handler(request: NextApiRequest, response: NextApiResponse) {
 
-    const res = await fetch(
-        `${process.env.OCKAM_API_BASE_URL}/resolve_aws_customer`,
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'ockam-api-key': process.env.OCKAM_API_KEY as string,
-            },
-            body: JSON.stringify(request.query)
-        })
-
-    const data = await res.json()
-    response.redirect(`/get-started?customer=${data.customer_id}&product=${data.product}`);
+    const params = request.body;
+    if ('x-amzn-marketplace-token' in params) {
+        const res = await fetch(
+            `${process.env.OCKAM_API_BASE_URL}/resolve_aws_customer`,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'ockam-api-key': process.env.OCKAM_API_KEY as string,
+                },
+                body: JSON.stringify(params)
+            })
+        if (res.status !== 200) {
+            response.redirect('/get-started');
+        } else {
+            const data = await res.json()
+            response.redirect(`/get-started?customer=${data.customer_id}&product=${data.product}`);
+        }
+    } else {
+        response.redirect('/get-started');
+    }
 }
