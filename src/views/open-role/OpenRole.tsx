1fff22ca46f5ad645b701b4cb68eaf16490c6ab7
Mateusz Witkowski <mateusz.w@masterborn.com>
2024-07-24 14:56:14 +0200

    feat: update offer page

--- src/views/open-role/OpenRole.tsx
+++ src/views/open-role/OpenRole.tsx
@@ -1,30 +1,41 @@
-import { FunctionComponent, useContext } from 'react';
+import { FunctionComponent } from 'react';
 import {
   Box,
   Button,
-  Container,
   Flex,
   Heading,
   List,
   ListItem,
   ListItemProps,
   Icon,
+  Stack,
+  Text,
 } from '@chakra-ui/react';
 import { ArrowBackIcon } from '@chakra-ui/icons';
 import parse, { domToReact, HTMLReactParserOptions } from 'html-react-parser';
 
 import { OPEN_ROLES_PATH } from '@consts/paths';
 import { LeverPosting, LeverPostingList } from '@typings/lever';
-import GreenIconWrapper from '@components/GreenIconWrapper';
 import CheckIcon from '@assets/icons/check.svg';
-import { MobileNavbarContext } from '@root/contextProviders/MobileNavbarProvider';
+
+import JoinTheTeamImage from './assets/join-the-team.svg';
 
 const ListItemWrapper: FunctionComponent<ListItemProps> = ({ children, ...restProps }) => (
-  <ListItem display="flex" {...restProps}>
-    <GreenIconWrapper as="span" w={6} h={6} mr={3}>
-      <Icon as={CheckIcon} color="white" w="full" h="full" />
-    </GreenIconWrapper>
-    <Box as="span" flex={1}>
+  <ListItem display="flex" alignItems="center" justifyContent="center" {...restProps}>
+    <Flex
+      alignItems={{ base: 'baseline', lg: 'center' }}
+      justifyContent="center"
+      flexShrink={0}
+      borderRadius="100%"
+      bg="rgba(116, 223, 255, 0.30)"
+      as="span"
+      w="0.75rem"
+      h="0.75rem"
+      mr="0.5rem"
+    >
+      <Icon as={CheckIcon} color="brand.600" w="full" h="full" />
+    </Flex>
+    <Box as="span" flex={1} fontSize="1rem" color="brand.800">
       {children}
     </Box>
   </ListItem>
@@ -44,7 +55,14 @@ const DetailsList: FunctionComponent<LeverPostingList> = ({ text, content }): JS
 
   return (
     <Box pb={16} key={text}>
-      <Heading as="h3" fontSize="4xl" mb={{ base: 10, lg: 12 }}>
+      <Heading
+        as="h2"
+        fontFamily="neutraface"
+        fontSize={{ base: '2.5rem' }}
+        lineHeight={1.3}
+        mb={{ base: '1.5rem' }}
+        fontWeight={700}
+      >
         {text}
       </Heading>
       <List spacing={{ base: 6, lg: 7 }}>{parsedListItems}</List>
@@ -55,16 +73,15 @@ const DetailsList: FunctionComponent<LeverPostingList> = ({ text, content }): JS
 type OpenRoleProps = { openRole: LeverPosting };
 
 const OpenRole: FunctionComponent<OpenRoleProps> = ({ openRole }) => {
-  const { isBelowSmallLaptop } = useContext(MobileNavbarContext);
-
   const ApplyButton = (
     <Button
+      maxW={{ base: '18.5rem', lg: '10rem' }}
+      w="100%"
       as="a"
       href={openRole.applyUrl}
+      h="3.5rem"
+      variant="primary"
       target="_blank"
-      colorScheme="avocado"
-      color="black"
-      mt={{ base: 10, lg: 0 }}
     >
       Apply
     </Button>
@@ -81,63 +98,91 @@ const OpenRole: FunctionComponent<OpenRoleProps> = ({ openRole }) => {
   };
 
   return (
-    <Container variant="section" alignItems="flex-start">
-      <Box maxW="3xl" pb={16} pt={isBelowSmallLaptop ? 0 : 12}>
-        <Button size="lg" color="brand.900" as="a" href={OPEN_ROLES_PATH} variant="link" mb={10}>
-          <ArrowBackIcon w={6} h={6} mr={2} /> Back to Job Positions
-        </Button>
+    <Box
+      maxW="70rem"
+      mx="auto"
+      px={{ base: '0.75rem' }}
+      pt={{ base: '3.5rem', lg: '6.25rem' }}
+      pb={{ base: '5rem', lg: '10rem' }}
+    >
+      <Button
+        as="a"
+        fontSize={{ base: '0.875rem', lg: '1rem' }}
+        fontWeight={600}
+        href={OPEN_ROLES_PATH}
+        color="brand.500"
+        variant="link"
+        mb="3.5rem"
+      >
+        <ArrowBackIcon w={6} h={6} mr={2} /> Back to all positions
+      </Button>
 
-        <Flex
-          direction={{ base: 'column', lg: 'row' }}
-          align={{ lg: 'center' }}
-          justifyContent={{ lg: 'space-between' }}
-          mb={{ base: 16, lg: 24 }}
+      <Flex
+        direction={{ base: 'column', lg: 'row' }}
+        align={{ lg: 'center' }}
+        gap={{ base: '2rem' }}
+        justifyContent={{ lg: 'space-between' }}
+        mb={{ base: '2rem', lg: '6rem' }}
+      >
+        <Heading
+          as="h1"
+          textAlign={{ base: 'center', lg: 'left' }}
+          lineHeight={0.95}
+          fontFamily="neutraface"
+          fontWeight={700}
+          fontSize={{ base: '2.5rem', lg: '5.5rem' }}
         >
-          <Box position="relative" flex={1}>
-            <Heading as="h2" size="h2" lineHeight={1.3}>
-              {openRole.text}
-            </Heading>
+          {openRole.text}
+        </Heading>
 
-            <Box
-              position="absolute"
-              bottom={-3}
-              left={0}
-              maxW="3xs"
-              w="100%"
-              h="0.25rem"
-              bgColor="avocado.500"
-              borderRadius="md"
-            />
-          </Box>
+        {ApplyButton}
+      </Flex>
 
-          {ApplyButton}
-        </Flex>
+      <Box
+        fontSize="1.125rem"
+        color="gray.500"
+        lineHeight={1.4}
+        mb={{ base: '3.5rem', lg: '5rem' }}
+      >
+        <Box dangerouslySetInnerHTML={{ __html: openRole.description }} />
+      </Box>
 
-        <Box fontSize={{ lg: 'lg' }} color="gray.500" lineHeight={1.4} mb={{ base: 10, lg: 20 }}>
-          <Box dangerouslySetInnerHTML={{ __html: openRole.description }} />
-        </Box>
+      {openRole.lists.map(DetailsList)}
 
-        {openRole.lists.map(DetailsList)}
+      <Box fontSize="1.125rem" color="gray.500" lineHeight={1.4}>
+        <Box sx={boxStyles} dangerouslySetInnerHTML={{ __html: openRole.additional }} />
+      </Box>
 
-        <Box fontSize={{ lg: 'lg' }} color="gray.500" lineHeight={1.4}>
-          <Box sx={boxStyles} dangerouslySetInnerHTML={{ __html: openRole.additional }} />
-        </Box>
+      <Flex
+        mt={{ base: '3.5rem', lg: '5rem' }}
+        direction={{ base: 'column', lg: 'row' }}
+        align={{ base: 'center', lg: 'center' }}
+        p={{ base: '1rem', lg: '1.5rem' }}
+        border="1px solid"
+        borderColor="gray.100"
+        borderRadius="0.75rem"
+        gap={{ base: '2.5rem' }}
+      >
+        <Box as={JoinTheTeamImage} w="100%" maxW={{ base: '15rem', lg: '12rem' }} />
 
-        <Box maxW="md" h="1px" bgColor="gray.200" my={{ base: 10, lg: 20 }} />
-
-        <Flex
-          direction={{ base: 'column', lg: 'row' }}
-          align={{ lg: 'center' }}
-          justify={{ lg: 'space-between' }}
-          pb={16}
-        >
-          <Heading as="h3" fontSize="4xl">
+        <Stack textAlign={{ base: 'center', lg: 'left' }} mr={{ base: 'initial', lg: 'auto' }}>
+          <Heading
+            as="h3"
+            fontSize={{ base: '2.5rem' }}
+            fontWeight={700}
+            lineHeight={1.3}
+            fontFamily="neutraface"
+            color="brand.900"
+          >
             Join the Ockam Team
           </Heading>
-          {ApplyButton}
-        </Flex>
-      </Box>
-    </Container>
+          <Text lineHeight={1.3} fontSize={{ base: '1.125rem' }}>
+            and start creating tools developers will love
+          </Text>
+        </Stack>
+        {ApplyButton}
+      </Flex>
+    </Box>
   );
 };
 
