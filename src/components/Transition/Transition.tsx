70810c554b4158ff85e58e89e04646e56e21553a
Glenn Gillen <glenn@ockam.io>
2024-08-12 20:30:23 +1000

    refactor: remove unused animation and correct import

--- src/components/Transition/Transition.tsx
+++ src/components/Transition/Transition.tsx
@@ -1,5 +1,5 @@
 import React, { FC, ReactNode, useRef } from 'react';
-import { animated, useSpring } from 'react-spring';
+import { animated, useSpring } from '@react-spring/web';
 
 import useEffectOnce from '@root/hooks/useEffectOnce';
 
